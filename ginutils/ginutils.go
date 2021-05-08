6ee5e866fc9d967c140c84ae9bcf637f52830cf7
andrew <crazydiamond_xu@163.com>
2021-05-08 14:44:20 +0800

    update

--- ginutils/ginutils.go
+++ ginutils/ginutils.go
@@ -6,6 +6,6 @@ func InitGin() *gin.Engine {
 	r := gin.Default()
 	r.GET("ping", GetHander())
 	r.POST("create", postPerson())
-	r.GET("art", getTdxData())
+	r.GET("art", getTdxData()) //http://127.0.0.1:8080/art?code=000725&parameter=22
 	return r
 }
